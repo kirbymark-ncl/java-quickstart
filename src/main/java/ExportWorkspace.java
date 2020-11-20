815879616d3352218c25f2d204ddebe043000176
kirbymark-ncl <mkirby@ncl.com>
2020-11-20 15:04:54 -0500

    mark trying java

--- src/main/java/ExportWorkspace.java
+++ src/main/java/ExportWorkspace.java
@@ -10,19 +10,16 @@ import com.structurizr.util.WorkspaceUtils;
 import com.structurizr.view.*;
 import java.io.*;
 
-/**
- * This is a simple example of how to get started with Structurizr for Java.
- */
-public class Structurizr {
 
-    private static final long WORKSPACE_ID = 60892;
-    private static final String API_KEY = "f3c621ea-9c22-44bb-aef0-bedad45cc994";
-    private static final String API_SECRET = "8190760c-c480-4624-bcf6-dfdd3c046288";
+public class ExportWorkspace {
+
+    private static final long WORKSPACE_ID = 36141;
+    private static final String API_KEY = "key";
+    private static final String API_SECRET = "secret";
 
     public static void main(String[] args) throws Exception {
-        // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
         StructurizrClient structurizrClient = new StructurizrClient(API_KEY, API_SECRET);
         WorkspaceUtils.saveWorkspaceToJson(structurizrClient.getWorkspace(WORKSPACE_ID), new File("workspace.json"));
-
     }
+
 }
